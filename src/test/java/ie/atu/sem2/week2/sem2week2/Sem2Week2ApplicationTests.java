f6f0252a6cfbec96a5136270e5da01ce28c9bfbb
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-07 15:13:21 +0000

    Created Project

---
+++ src/test/java/ie/atu/sem2/week2/sem2week2/Sem2Week2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.sem2.week2.sem2week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sem2Week2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
