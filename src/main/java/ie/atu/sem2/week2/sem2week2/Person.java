91e1d71d119e6f424ccf561618069ddee81cf60d
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-07 16:30:45 +0000

    Added New Endpoints For Details & Person

---
+++ src/main/java/ie/atu/sem2/week2/sem2week2/Person.java
@@ -0,0 +1,27 @@
+package ie.atu.sem2.week2.sem2week2;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = "";
+        this.age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/ie/atu/sem2/week2/sem2week2/RequestController.java
+++ src/main/java/ie/atu/sem2/week2/sem2week2/RequestController.java
@@ -1,21 +1,28 @@
 package ie.atu.sem2.week2.sem2week2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class RequestController {
     @GetMapping("/hello")
-    public String hello()
-    {
+    public String hello() {
         return "Hello World...";
     }
 
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "Hello " + name;
+    }
+
+    @GetMapping("/details")
+    public String details(@RequestParam String name, @RequestParam int age) {
+        return "Name : " + name + ",  Age : " + age;
+    }
+
+    @GetMapping("/person")
+    public Person getPerson() {
+        Person person = new Person("Conor", 21);
+        return person;
     }
 }
