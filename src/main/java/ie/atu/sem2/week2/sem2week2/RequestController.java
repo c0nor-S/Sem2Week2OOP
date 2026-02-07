e4db6d18c78f2e10230aa04bb7e37279af3fab06
c0nor-S <150271462+c0nor-S@users.noreply.github.com>
2026-02-07 16:32:53 +0000

    Revert "Added New Endpoints For Details & Person"
    This reverts commit 63d795738173b942cb3f5e0be4df4147d05f10a3.

--- src/main/java/ie/atu/sem2/week2/sem2week2/RequestController.java
+++ src/main/java/ie/atu/sem2/week2/sem2week2/RequestController.java
@@ -1,28 +1,21 @@
 package ie.atu.sem2.week2.sem2week2;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
 public class RequestController {
     @GetMapping("/hello")
-    public String hello() {
+    public String hello()
+    {
         return "Hello World...";
     }
 
     @GetMapping("/greet/{name}")
     public String greetByName(@PathVariable String name) {
         return "Hello " + name;
-    }
-
-    @GetMapping("/details")
-    public String details(@RequestParam String name, @RequestParam int age) {
-        return "Name : " + name + ",  Age : " + age;
-    }
-
-    @GetMapping("/person")
-    public Person getPerson() {
-        Person person = new Person("Conor", 21);
-        return person;
     }
 }
